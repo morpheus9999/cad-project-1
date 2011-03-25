eb9f7c3c3490d46f5091ff2c7be7917aad90dc91
morpheus9999 <morpheus9999@43616ab2-9758-a190-8fa0-8e37f8a27fb7>
2011-03-25 01:45:50 +0000

    Start

---
+++ Projecto 1/main1.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main1.cpp
+ * Author: jojo
+ *
+ * Created on 25 de Março de 2011, 1:43
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
